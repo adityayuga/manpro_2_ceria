02e655f9171aa7a705bcd7d830cd4d54186eed5d
Ega Prathama <ega.prathama@ti.ukdw.ac.id>
2016-03-22 15:52:04 +0700

    about sementara

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -1,13 +1,49 @@
 @extends('layout.layout')
 
+@section('css')
+<!--css here-->
+<link href="{{ asset('cssabout.css') }}" rel="stylesheet" type="text/css">
+@stop
+
 @section('content')
 
     <!-- Main Content -->
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum molestiae debitis nobis, quod sapiente qui voluptatum, placeat magni repudiandae accusantium fugit quas labore non rerum possimus, corrupti enim modi! Et.</p>
+                <h1>Lokasi Kami</h1>
+                <p>Universitas Kristen Duta Wacana</p>
+                <h1>Kontak</h1>
+                <li>Phone	: 0274-553929</li>
+                <li>Fax		: 0274-513235</li>
+                <li>HP		: 0274-3154236</li>
+                <li>Email	: pppukdw@yahoo.com</li>
+                <h1>Info</h1>
+                <li>Waktu layanan hari Senin - Jumat pukul 08.00 - 15.00</li>
+                <li>Informasi dan permintaan layanan dapat dilakukan melalui telepon, email, fax, atau DATANG langsung ke kantor PPP UKDW</li>
+                <li>Libur tahunan tanggal 23 Desember sampai 3 Januari</li>
+                <h1>Jadwal Konselor</h1>
+                <table class="tablebordered">
+                	<tr>
+                		<td>Konselor</td>
+                		<td>Jadwal Konselor</td>
+                	</tr>
+                	<tr>
+                		<td>Nevi Kurnia Arianti, M.si, P.si.*</td>
+                		<td>Selasa, pk 10.00-12.00</td>
+                	</tr>
+                	<tr>
+                		<td></td>
+                		<td>Kamis, pk 10.00-12.00</td>
+                	</tr>
+                	<tr>
+                		<td>Imelda, M.Psi.*</td>
+                		<td>Kamis, pk 10.00-12.00</td>
+                	</tr>
+                	<tr>
+                		<td></td>
+                		<td>Jumat, pk 10.00-12.00</td>
+                	</tr>
+                </table>
             </div>
         </div>
 @stop
