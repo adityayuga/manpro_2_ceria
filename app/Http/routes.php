b19e59583dfc0e4818d3f666b8e6c4a0ea8ef385
adityayuga <yugaapik@gmail.com>
2016-03-22 13:28:56 +0700

    edit route, cegah registrasi

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,13 +25,21 @@ Route::get('/', function () {
 | kernel and includes session state, CSRF protection, and more.
 |
 */
-
-Route::group(['middleware' => ['web']], function () {
-    //
-});
-
 Route::group(['middleware' => 'web'], function () {
-    Route::auth();
+    //Route::auth();
+	//-------------------------------------AUTH------------------------------------//
+	Route::get('login', ['as' => 'auth.login', 'uses' => 'Auth\AuthController@showLoginForm']);
+	Route::post('login', ['as' => 'auth.login', 'uses' => 'Auth\AuthController@login']);
+	Route::get('logout', ['as' => 'auth.logout', 'uses' => 'Auth\AuthController@logout']);
+
+	// Registration Routes...
+	//Route::get('register', ['as' => 'auth.register', 'uses' => 'Auth\AuthController@showRegistrationForm']);
+	//Route::post('register', ['as' => 'auth.register', 'uses' => 'Auth\AuthController@register']);
+
+	// Password Reset Routes...
+	Route::get('password/reset/{token?}', ['as' => 'auth.password.reset', 'uses' => 'Auth\PasswordController@showResetForm']);
+	Route::post('password/email', ['as' => 'auth.password.email', 'uses' => 'Auth\PasswordController@sendResetLinkEmail']);
+	Route::post('password/reset', ['as' => 'auth.password.reset', 'uses' => 'Auth\PasswordController@reset']);
 
     Route::get('/home', 'HomeController@index');
 });
