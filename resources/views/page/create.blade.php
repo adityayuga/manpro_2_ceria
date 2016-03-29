b716edf92389589375192077c95dcd2972bfa491
Yohanes <yohanes@ti.ukdw.ac.id>
2016-03-29 14:18:12 +0700

    integrasi JSON dengan summerNote

--- resources/views/page/create.blade.php
+++ resources/views/page/create.blade.php
@@ -46,7 +46,7 @@
                     <div id="success"></div>
                     <div class="row">
                         <div class="form-group col-xs-12">
-                            <button type="submit" class="btn btn-default">Send</button>
+                            <button type="submit" id="btnSubmit" class="btn btn-default">Send</button>
                         </div>
                     </div>
                 </form>
