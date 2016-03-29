6641d0ac720d2c28fc3c61a048bdfb6be9884052
adityayuga <yugaapik@gmail.com>
2016-03-29 13:26:01 +0700

    merge solve conflict

--- js/artikel/create_artikel.js
+++ js/artikel/create_artikel.js
@@ -1,15 +1,12 @@
 $(document).ready(function(){
-<<<<<<< HEAD
 	$('#isi_artikel').summernote({
 		height: 300,
 		minHeight: 400,
 		maxHeight: 400,
 		focus: true
 	});
-=======
 	
 	$('#isi_artikel').summernote('code', "hello sam er nod");
->>>>>>> e566888e7e09c8ddd3b47374163a2d30775c4a50
 
 	
 
