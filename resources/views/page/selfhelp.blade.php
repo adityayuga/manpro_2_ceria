503415fd951feaf46639a6d0938a81621f039426
Rico Manurung <rico.manurung@ti.ukdw.ac.id>
2016-04-30 15:27:53 +0700

    perbaikan tampilan minor

--- resources/views/page/selfhelp.blade.php
+++ resources/views/page/selfhelp.blade.php
@@ -27,6 +27,7 @@ active
     <div class="container">
         <div class="row">
              <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <h1 class="post-title">Artikel Self Help</h2><hr>
                     @foreach ($artikel as $a_artikel)
                 <div class="post-preview">
                     <a href="{{ URL::to('selfhelp/' . $a_artikel->slug) }}" value="<?=$a_artikel->slug?>">
@@ -40,7 +41,7 @@ active
                             <?PHP }?>
                             <td align="left">
                                 <h2 class="post-title responsivetexth2">
-                            {{$a_artikel->judul}}
+                            {!!str_limit($a_artikel->judul, 30,"...")!!}
                                 </h2>
                             <p class="post-subtitle responsivetexth3">
                             {!!str_limit($a_artikel->deskripsi,60,"...")!!}
@@ -48,7 +49,12 @@ active
                         </tr>
                         </table>
                     </a>
-                    <p class="post-meta"><!-- kategori artikel dari database-->Category : {{ucfirst($a_artikel->kategori)}} Posted on {{$a_artikel->created_at}}</p>
+                    <?php if ($a_artikel->kategori == "umum") {?>
+                        <p class="post-meta"><!-- kategori artikel dari database--><b>Category </b> <a href="{{URL::to('umum')}}">{{ucfirst($a_artikel->kategori)}}</a> <b>Posted on </b> {{$a_artikel->created_at}}</p>
+                    <?php } 
+                       else{ ?>
+                        <p class="post-meta"><!-- kategori artikel dari database--><b>Category </b> <a href="{{ $a_artikel->kategori }}">{{ucfirst($a_artikel->kategori)}}</a> <b>Posted on </b> {{$a_artikel->created_at}}</p>
+                     <?php } ?>
                 </div>
                 <hr>
                     @endforeach
